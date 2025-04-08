79078ac797da34dea2eaffa1d781661935328f62
Melik Unesi <melikdev@gmail.com>
2025-04-09 01:44:15 +0300

    authjs with resend and prisma and added a post model to prisma file

---
+++ src/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
+import Resend from 'next-auth/providers/resend';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [Resend],
+});
